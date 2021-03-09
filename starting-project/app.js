5484f775875574770c299b33f13df8ee26f2a1d7
ruddy palvair <r.palvair@gmail.com>
2021-03-09 12:33:36 +0100

    feat : App with VueJs

--- starting-project/app.js
+++ starting-project/app.js
@@ -1,12 +1,28 @@
-const buttonEl = document.querySelector("button")
-const inputEl = document.querySelector("input")
-const listEl = document.querySelector("ul")
+const App = {
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    }
+  },
+  methods: {
+    addGoal() {
+      console.log("addGoal")
+      this.goals.push(this.enteredValue)
+    },
+  },
+}
+Vue.createApp(App).mount("#app")
 
-buttonEl.addEventListener("click", () => {
-  const enteredValue = inputEl.value
-  console.log("enteredValue", enteredValue)
-  const listItemEl = document.createElement("li")
-  listItemEl.textContent = enteredValue
-  listEl.appendChild(listItemEl)
-  inputEl.value = ""
-})
+// const buttonEl = document.querySelector("button")
+// const inputEl = document.querySelector("input")
+// const listEl = document.querySelector("ul")
+
+// buttonEl.addEventListener("click", () => {
+//   const enteredValue = inputEl.value
+//   console.log("enteredValue", enteredValue)
+//   const listItemEl = document.createElement("li")
+//   listItemEl.textContent = enteredValue
+//   listEl.appendChild(listItemEl)
+//   inputEl.value = ""
+// })
