35a53d1646477f1e7687b75e400953de7c021a40
ruddy palvair <r.palvair@gmail.com>
2021-03-15 19:20:14 +0100

    feat: Two apps in a single html file

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -1,18 +1,27 @@
 const app = Vue.createApp({
   data() {
     return {
-      currentUserInput: '',
-      message: 'Vue is great!',
-    };
+      currentUserInput: "",
+      message: "Vue is great!",
+    }
   },
   methods: {
     saveInput(event) {
-      this.currentUserInput = event.target.value;
+      this.currentUserInput = event.target.value
     },
     setText() {
-      this.message = this.currentUserInput;
+      this.message = this.currentUserInput
     },
   },
-});
+})
 
-app.mount('#app');
+app.mount("#app")
+
+const app2 = Vue.createApp({
+  data() {
+    return {
+      favoriteMeal: "Salad",
+    }
+  },
+})
+app2.mount("#app2")
