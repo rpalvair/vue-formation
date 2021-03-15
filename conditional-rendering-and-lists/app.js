514d827f5c6ccc99a09f583b0459abfec16a5597
ruddy palvair <r.palvair@gmail.com>
2021-03-15 09:41:26 +0100

    feat: Display message with "v-if" if goals array is empty feat : Add goal on user click

--- conditional-rendering-and-lists/app.js
+++ conditional-rendering-and-lists/app.js
@@ -1,7 +1,15 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return { goals: ["Finish the course!"], enteredGoalValue: null }
   },
-});
+  methods: {
+    addGoal() {
+      if (this.enteredGoalValue) {
+        console.log("add goal",this.enteredGoalValue)
+        this.goals.push(this.enteredGoalValue)
+      }
+    },
+  },
+})
 
-app.mount('#user-goals');
+app.mount("#user-goals")
