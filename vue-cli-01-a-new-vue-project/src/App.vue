8a047e35d2dc3ed7489cd7f3087e10647b5d6c6b
ruddy palvair <r.palvair@gmail.com>
2021-03-16 12:01:34 +0100

    feat/vue-cli (#7)

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <section>
+    <header>
+      <h1>My Friends</h1>
+    </header>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+import FriendContact from "./components/FriendContact.vue"
+export default {
+  components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "09876 543 221",
+          email: "julie@localhost.com",
+        },
+      ],
+    }
+  },
+}
+</script>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Jost&display=swap");
+</style>
