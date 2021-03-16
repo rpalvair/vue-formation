c86947dd7f67f05d6d948b1e6ef104c03307a105
ruddy palvair <r.palvair@gmail.com>
2021-03-16 11:41:28 +0100

    feat : Create a basic Vue App

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<template> 
+<section>
+    <h2>My Friends</h2>
+    <ul>
+        <li v-for="friend in friends" :key="friend.id">{{friend.name}}</li>
+    </ul>
+</section>
+</template>
+
+<script>
+export default  {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "09876 543 221",
+          email: "julie@localhost.com",
+        },
+      ],
+    }
+  },
+}
+</script>
