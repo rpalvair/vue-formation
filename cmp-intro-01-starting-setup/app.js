5390f591fec016b5d3c7a6983a774a98c24cd66d
ruddy palvair <r.palvair@gmail.com>
2021-03-16 10:14:53 +0100

    feat : Toggle details for elements in for loop

--- cmp-intro-01-starting-setup/app.js
+++ cmp-intro-01-starting-setup/app.js
@@ -1,23 +1,27 @@
 const app = Vue.createApp({
   data() {
     return {
-        friends: [
-            {
-                id:'manuel',
-                name:'Manuel Lorenz',
-                phone:'01234 5678 991',
-                email:'manuel@localhost.com'
-
-            },
-            {
-                id:'julie',
-                name:'Julie Jones',
-                phone:'09876 543 221',
-                email:'julie@localhost.com'
-
-            }
-        ]
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "09876 543 221",
+          email: "julie@localhost.com",
+        },
+      ],
+      detailsAreVisible: false,
     }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible
+    },
   },
 })
 
