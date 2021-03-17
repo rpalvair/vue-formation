40186137201a3f4cc001cd8b84c470cf5b589312
ruddy palvair <r.palvair@gmail.com>
2021-03-17 16:47:47 +0100

    feat/diving-deeper-in-components (#9)

---
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue"
+import ActiveGoals from "./components/ActiveGoals"
+import ManageGoals from "./components/ManageGoals"
+
+export default {
+  components: {
+    TheHeader,
+    ActiveGoals,
+    ManageGoals,
+  },
+  data() {
+    return {
+      selectedComponent: "active-goals",
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    }
+  },
+  methods: {
+    setSelectedComponent(component) {
+      this.selectedComponent = component
+    },
+  },
+}
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
