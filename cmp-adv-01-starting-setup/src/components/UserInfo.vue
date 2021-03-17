072fbb9558994151a31fd2a52217a6ec2065259e
ruddy palvair <r.palvair@gmail.com>
2021-03-17 14:59:26 +0100

    feat : Use slot to receive htlm code in a component template

--- cmp-adv-01-starting-setup/src/components/UserInfo.vue
+++ cmp-adv-01-starting-setup/src/components/UserInfo.vue
@@ -1,28 +1,22 @@
 <template>
-  <section>
+  <base-card>
     <div>
       <h3>{{ fullName }}</h3>
       <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
     </div>
-    <p>{{ infoText }}</p>
-  </section>
+    <p>{{ infoText }}</p></base-card
+  >
 </template>
 
 <script>
+import BaseCard from "./BaseCard.vue"
 export default {
-  props: ['fullName', 'infoText', 'role'],
-};
+  components: { BaseCard },
+  props: ["fullName", "infoText", "role"],
+}
 </script>
 
 <style scoped>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
-
 section header {
   display: flex;
   justify-content: space-between;
