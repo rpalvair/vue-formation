bd112bca5f61f6abf898553277de19f66ea50a07
ruddy palvair <r.palvair@gmail.com>
2021-03-17 15:14:48 +0100

    feat : Use named slots and 'template'

--- cmp-adv-01-starting-setup/src/components/UserInfo.vue
+++ cmp-adv-01-starting-setup/src/components/UserInfo.vue
@@ -1,12 +1,14 @@
 <template>
   <section>
     <base-card>
-      <header>
+      <template v-slot:header>
         <h3>{{ fullName }}</h3>
         <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-      </header>
-      <p>{{ infoText }}</p></base-card
-    >
+      </template>
+      <template v-slot:default>
+        <p>{{ infoText }}</p>
+      </template>
+    </base-card>
   </section>
 </template>
 
