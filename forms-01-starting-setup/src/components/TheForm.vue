85224c4dd0bf6376a80bdfa27c102449f4f8e7bf
ruddy palvair <r.palvair@gmail.com>
2021-03-18 12:17:24 +0100

    feat: 'number' modifier to convert input value to number

--- forms-01-starting-setup/src/components/TheForm.vue
+++ forms-01-starting-setup/src/components/TheForm.vue
@@ -1,12 +1,12 @@
 <template>
-  <form>
+  <form @submit.prevent="submitForm">
     <div class="form-control">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" />
+      <input id="user-name" name="user-name" type="text" v-model="userName" />
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number" />
+      <input id="age" name="age" type="number" v-model.number="userAge"/>
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
@@ -51,6 +51,23 @@
     </div>
   </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: '',
+      userAge: null
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log(this.userAge);
+      console.log(31);
+    }
+  }
+};
+</script>
 
 <style scoped>
 form {
