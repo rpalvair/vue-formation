e0c27c63df4c84365dd77fdcc92eac7220b98b75
ruddy palvair <r.palvair@gmail.com>
2021-03-19 18:30:35 +0100

    feat: Redirect

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -9,6 +9,7 @@ const app = createApp(App);
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    { path: '/', redirect: '/teams' },
     { path: '/teams', component: TeamsList },
     { path: '/users', component: UsersList },
     { path: '/teams/:id', component: TeamMembers, props: true }
