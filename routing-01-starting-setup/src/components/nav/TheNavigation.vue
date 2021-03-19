f50db34e7f7216528696779a521daaa265825be0
ruddy palvair <r.palvair@gmail.com>
2021-03-19 13:39:36 +0100

    feat: Use router-link to navigate

--- routing-01-starting-setup/src/components/nav/TheNavigation.vue
+++ routing-01-starting-setup/src/components/nav/TheNavigation.vue
@@ -3,10 +3,10 @@
     <nav>
       <ul>
         <li>
-          <button @click="setActivePage('teams-list')">Teams</button>
+          <router-link to="/teams">Teams</router-link>
         </li>
         <li>
-          <button @click="setActivePage('users-list')">Users</button>
+          <router-link to="/users">Users</router-link>
         </li>
       </ul>
     </nav>
@@ -14,14 +14,7 @@
 </template>
 
 <script>
-export default {
-  emits: ['set-page'],
-  methods: {
-    setActivePage(page) {
-      this.$emit('set-page', page);
-    },
-  },
-};
+export default {};
 </script>
 
 <style scoped>
@@ -49,18 +42,18 @@ li {
   margin: 0 2rem;
 }
 
-button {
-  font: inherit;
+a {
   background: transparent;
   border: 1px solid transparent;
   cursor: pointer;
   color: white;
   padding: 0.5rem 1.5rem;
   display: inline-block;
+  text-decoration: none;
 }
 
-button:hover,
-button:active {
+a:hover,
+a:active {
   color: #f1a80a;
   border-color: #f1a80a;
   background-color: #1a037e;
