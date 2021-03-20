59fd55facb923bdfa5f14db160ab8e33009b5526
ruddy palvair <r.palvair@gmail.com>
2021-03-20 11:14:24 +0100

    feat : Hook 'beforeRouteLeave' in a component

--- routing-01-starting-setup/src/components/users/UsersList.vue
+++ routing-01-starting-setup/src/components/users/UsersList.vue
@@ -1,7 +1,13 @@
 <template>
-<button @click="confirm">Confirm</button>
+  <button @click="confirm">Confirm</button>
+  <button @click="saveChanges">Save changes</button>
   <ul>
-    <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
+    <user-item
+      v-for="user in users"
+      :key="user.id"
+      :name="user.fullName"
+      :role="user.role"
+    ></user-item>
   </ul>
 </template>
 
@@ -13,16 +19,35 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  data() {
+    return {
+      changesSaved: false,
+    };
+  },
   methods: {
     confirm() {
-      this.$router.push('/teams')
+      this.$router.push('/teams');
+    },
+    saveChanges() {
+      this.changesSaved = true;
+    },
+  },
+  beforeRouteEnter(to, from, next) {
+    console.log('BeforeRouterEnter UsersList');
+    console.log(to, from);
+    next();
+  },
+  beforeRouteLeave(to, from, next) {
+    console.log('BeforeRouteLeave UsersList');
+    console.log(to, from);
+    if (this.changesSaved) {
+      next();
+    } else {
+      const userWantsToLeave = confirm('Are you sure? You got unsaved changes!');
+      console.log('userWantsToLeave', userWantsToLeave);
+      next(userWantsToLeave);
     }
   },
-  beforeRouteEnter(to,from,next) {
-    console.log("BeforeRouterEnter UsersList")
-    console.log(to,from)
-    next()
-  }
 };
 </script>
 
