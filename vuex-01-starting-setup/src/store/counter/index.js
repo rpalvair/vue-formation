311258be7b9eb6696a109199a519939f9b9d1c72
ruddy palvair <r.palvair@gmail.com>
2021-03-23 14:21:46 +0100

    feat : Structuring vuex code and files

---
+++ vuex-01-starting-setup/src/store/counter/index.js
@@ -0,0 +1,18 @@
+
+import mutations from './mutations'
+import actions from './actions'
+import getters from './getters'
+
+const counterModule = {
+    namespaced: true,
+    state() {
+        return {
+            counter: 0,
+        }
+    },
+    mutations,
+    actions,
+    getters
+}
+
+export default counterModule
