e14f7605e45cef72b1392b840995581ac11962e5
ruddy palvair <r.palvair@gmail.com>
2021-03-24 12:11:53 +0100

    feat : Add and use store

--- main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -7,10 +7,16 @@
     </div>
     <ul>
       LIST OF COACHES
+      <li v-for="coach in coaches" :key="coach.id">{{ coach }}</li>
     </ul>
   </section>
 </template>
 
 <script>
-export default {};
+import { mapGetters } from 'vuex';
+export default {
+  computed: {
+    ...mapGetters('coaches', ['coaches']),
+  },
+};
 </script>
