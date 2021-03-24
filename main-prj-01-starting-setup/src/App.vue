ad021cc3a62701f166f799f7c163b6fb88b5493b
ruddy palvair <r.palvair@gmail.com>
2021-03-24 11:33:06 +0100

    feat : Add the header

--- main-prj-01-starting-setup/src/App.vue
+++ main-prj-01-starting-setup/src/App.vue
@@ -1,9 +1,13 @@
 <template>
+  <the-header></the-header>
   <router-view></router-view>
 </template>
 
 <script>
-export default {};
+import TheHeader from './components/TheHeader.vue';
+export default {
+  components: { TheHeader },
+};
 </script>
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
