3e1c408194b9a4770405a5b5301cda68bdfe5d11
ruddy palvair <r.palvair@gmail.com>
2021-03-25 16:25:31 +0100

    feat : Save coach in firebase(from vuex action)

--- main-prj-01-starting-setup/src/store/modules/coaches/actions.js
+++ main-prj-01-starting-setup/src/store/modules/coaches/actions.js
@@ -1,12 +1,26 @@
+import config from '../../../config'
 export default {
 
-    saveCoach(context, payload) {
+    async saveCoach(context, payload) {
         console.log("saveCoach", context)
         console.log("payload", payload)
         const coaches = context.getters['coaches']
         console.log("length", coaches.length)
         const id = "c" + (coaches.length + 1)
-        context.commit('saveCoach', { ...payload, id })
-        context.dispatch('setId', { id }, { root: true })
+        const result = await fetch(`${config.firebaseUrl}/coaches/${id}.json`, {
+            method: 'PUT',
+            body: JSON.stringify({ ...payload, id })
+        })
+
+        const data = await result.json()
+        console.log("data", data)
+        if (result.ok) {
+            context.commit('saveCoach', { ...payload, id })
+            context.dispatch('setId', { id }, { root: true })
+        } else {
+            //
+        }
+
+
     }
 }
