83eccb74376806b51d709fdc9cd66b85ee90960a
ruddy palvair <r.palvair@gmail.com>
2021-03-25 19:12:20 +0100

    feat : Not found page

--- main-prj-01-starting-setup/src/pages/Nothing.vue
+++ main-prj-01-starting-setup/src/pages/Nothing.vue
@@ -1,3 +1,13 @@
 <template>
-    <h2>Oups! Nothing here!</h2>
+    <section>
+        <base-card>
+        <h2>
+            Page Not Found
+        </h2>
+        <p>
+            This page could not be found - maybe check out all our
+            <router-link to="/coaches">coaches</router-link>
+        </p>
+        </base-card>
+    </section>
 </template>
