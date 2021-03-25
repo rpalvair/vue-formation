a7df9d9313549fbd10077b90116290b6e0525d15
ruddy palvair <r.palvair@gmail.com>
2021-03-25 15:53:33 +0100

    feat : Display incoming requests

--- main-prj-01-starting-setup/src/pages/requests/RequestsReceived.vue
+++ main-prj-01-starting-setup/src/pages/requests/RequestsReceived.vue
@@ -1,9 +1,46 @@
 <template>
-    REQUESTS
+  <section>
+    <base-card>
+      <header>
+        <h2>Request Received</h2>
+      </header>
+      <ul>
+        <request-item v-for="request in requests" :email="request.userEmail" :message="request.message" :key="request.id">
+        </request-item>
+      </ul>
+      <h3 v-if="!hasRequests">You haven't received any requests yet!</h3>
+    </base-card>
+  </section>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+import RequestItem from './RequestItem';
 export default {
-    
-}
+  components: { RequestItem },
+  computed: {
+    ...mapGetters('requests', ['hasRequests']),
+    requests() {
+      const requests = this.$store.getters['requests/requests'];
+      console.log('requests', requests);
+      return requests;
+    },
+  },
+};
 </script>
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
