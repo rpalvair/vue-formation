6286c6a2083a72a60905765aafcbdd1876657752
ruddy palvair <r.palvair@gmail.com>
2021-03-30 15:47:12 +0200

    feat: Async component BaseDialog

--- main-prj-01-starting-setup/src/main.js
+++ main-prj-01-starting-setup/src/main.js
@@ -1,11 +1,14 @@
-import { createApp } from 'vue';
+import { createApp, defineAsyncComponent } from 'vue';
 import router from './router'
 import App from './App.vue'
 import store from './store/index'
 import BaseCard from './components/ui/BaseCard.vue'
 import BaseButton from './components/ui/BaseButton.vue'
 import BaseBadge from './components/ui/BaseBadge.vue'
-import BaseDialog from './components/ui/BaseDialog.vue'
+// import BaseDialog from './components/ui/BaseDialog.vue'
+import BaseSpinner from './components/ui/BaseSpinner'
+
+const BaseDialog = defineAsyncComponent(() => import('./components/ui/BaseDialog.vue'))
 
 const app = createApp(App);
 
@@ -16,6 +19,7 @@ app.use(store)
 app.component('base-card', BaseCard)
 app.component('base-button', BaseButton)
 app.component('base-badge', BaseBadge)
-app.component('base-dialog',BaseDialog)
+app.component('base-spinner', BaseSpinner)
+app.component('base-dialog', BaseDialog)
 
 app.mount('#app');
